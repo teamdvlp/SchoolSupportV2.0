f5895ebf398dc7be17c3b9147db0f7986a6b8c22
tiendang <tiendvlp@gmail.com>
2019-03-03 10:17:38 +0700

    - thêm asset

--- app/src/main/java/com/teamttdvlp/goodthanbefore/schoolsupport/MainActivity.kt
+++ app/src/main/java/com/teamttdvlp/goodthanbefore/schoolsupport/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.teamttdvlp.goodthanbefore.schoolsupport
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
