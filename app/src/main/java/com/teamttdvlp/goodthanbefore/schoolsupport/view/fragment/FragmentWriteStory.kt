f524d5369e8806f001dd6046468dd4d912dd6191
tangtaidvlp <tangtaidvlp@gmail.com>
2019-03-28 22:39:50 +0700

    Refactor Drawable - Add Logging Support Librarry

--- app/src/main/java/com/teamttdvlp/goodthanbefore/schoolsupport/view/fragment/FragmentWriteStory.kt
+++ app/src/main/java/com/teamttdvlp/goodthanbefore/schoolsupport/view/fragment/FragmentWriteStory.kt
@@ -32,6 +32,7 @@ class FragmentWriteStory : Fragment(), UploadAvatarEvent {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mViewModel = getViewModel()
+
     }
 
     override fun onCreateView(
